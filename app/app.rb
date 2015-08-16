23aa05fa5114a725aa06de689934cef1eaab71e2
Fadi-Hakim <Fadihakim@msn.com>
2015-08-16 22:34:24 +0100

    Saved user info when incorrect password confirmation entered

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
 require_relative '../data_mapper_setup'
+require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
 
 enable :sessions
 set :session_secret, 'super secret'
+register Sinatra::Flash
 
   get '/' do
     erb :index
@@ -25,6 +27,7 @@ set :session_secret, 'super secret'
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
@@ -32,9 +35,15 @@ set :session_secret, 'super secret'
     @user = User.create(email: params[:email],
                 password: params[:password],
                 name: params[:name],
-                username: params[:username])
-    session[:user_id] = @user.id
-    redirect to('/')
+                username: params[:username],
+                password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/peeps')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,16 +6,27 @@ feature 'User sign up' do
   end
 
   def sign_up(email: 'fadi@example.com',
-              password: 'oranges!',
               name: 'fadi',
-              username: 'fadi')
+              username: 'fadi',
+              password: '12345678',
+              password_confirmation: '12345678')
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email,    with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     fill_in :name,    with: name
     fill_in :username, with: username
     click_button 'Sign up'
   end
 
+  scenario 'requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
+  scenario 'with a password that does not match' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users') 
+    expect(page).to have_content 'Password and confirmation password do not match'
+  end
 end
